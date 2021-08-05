100891bc246aea7d278fe1e53b8525db40a5069f
Alexis <alexcatajay@gmail.com>
2021-08-05 18:05:58 +0800

    trying port

--- src/app.js
+++ src/app.js
@@ -5,7 +5,8 @@ const geocode = require('./utils/geocode')
 const forecast = require('./utils/forecast')
 
 const app = express()
-const port = process.env.PORT || 3000
+//const port = process.env.PORT || 3000
+const { PORT = 3000 } = process.env
 
 // Define paths for Express config
 const publicDirectoryPath = path.join(__dirname, '../public')
